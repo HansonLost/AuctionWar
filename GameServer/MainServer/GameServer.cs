b470707f61a63a91a462cc00711938f65240c5cb
卢俊廷 <576409191@qq.com>
2020-03-08 14:21:34 +0800

    新建 GameServer 项目

---
+++ GameServer/MainServer/GameServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainServer
+{
+    public class GameServer
+    {
+        public bool isShutdown { get; private set; }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Shutdown()
+        {
+
+        }
+    }
+}
---
+++ GameServer/MainServer/Launch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainServer
+{
+    class Launch
+    {
+        static void Main(string[] args)
+        {
+            GameServer server = new GameServer();
+            server.Start();
+            while (!server.isShutdown)
+            {
+                server.Update();
+            }
+            server.Shutdown();
+        }
+    }
+}
